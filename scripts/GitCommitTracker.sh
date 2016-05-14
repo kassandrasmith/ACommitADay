18e19b0cb605bf25f53ac96ad4aeaf9942d5ee65
kassandrasmith <kassandrasmith@utexas.edu>
2016-05-14 00:05:08 -0500

    Add functionality for OSX users

--- scripts/GitCommitTracker.sh
+++ scripts/GitCommitTracker.sh
@@ -16,10 +16,49 @@ do
 cd #not necessary, but a safeguard to get to root
   cd $data;
   cd .. #compensate for .git at end of line
-  DATE=$(date +%Y-%m-%d) &> /dev/null #Silence date output
+
   LOG_DATE=$(git log -1 HEAD --pretty=format:"%cd" --date=short)
-  StartDate=$(date -u -d "$DATE" +"%s")
-  FinalDate=$(date -u -d "$LOG_DATE" +"%s")
+
+  if [[ "$OSTYPE" == "linux-gnu" ]]; then
+
+            DATE=$(date +%Y-%m-%d) &> /dev/null #Silence date output
+            StartDate=$(date -d "$DATE" +"%s")
+            FinalDate=$(date -d "$LOG_DATE" +"%s")
+
+  elif [[ "$OSTYPE" == "darwin"* ]]; then
+          # Mac OSX
+            DATE=$(date +%Y-%m-%d) &> /dev/null #Silence date output
+            StartDate=$(date -jf "%Y-%m-%d" "$DATE" "+%s")
+            FinalDate=$(date -jf "%Y-%m-%d" "$LOG_DATE" "+%s")
+
+
+  elif [[ "$OSTYPE" == "cygwin" ]]; then
+          # POSIX compatibility layer and Linux environment emulation for Windows
+          echo cygwin not yet tested, results may be incorrect;
+          DATE=$(date +%Y-%m-%d) &> /dev/null #Silence date output
+          StartDate=$(date -d "$DATE" +"%s")
+          FinalDate=$(date -d "$LOG_DATE" +"%s")
+
+  elif [[ "$OSTYPE" == "msys" ]]; then
+          # Lightweight shell and GNU utilities compiled for Windows (part of MinGW)
+          echo mysys not yet tested, results may be incorrect;
+          DATE=$(date +%Y-%m-%d) &> /dev/null #Silence date output
+          StartDate=$(date -d "$DATE" +"%s")
+          FinalDate=$(date -d "$LOG_DATE" +"%s")
+
+  elif [[ "$OSTYPE" == "win32" ]]; then
+          # I'm not sure this can happen.
+          echo Win32 not supported.
+  elif [[ "$OSTYPE" == "freebsd"* ]]; then
+          # ...
+          DATE=$(date +%Y-%m-%d) &> /dev/null #Silence date output
+          StartDate=$(date -jf "%Y-%m-%d" "$DATE" "+%s")
+          FinalDate=$(date -jf "%Y-%m-%d" "$LOG_DATE" "+%s")
+  else
+          # Unknown.
+          echo
+  fi
+
   if [[ "$StartDate" -eq "$FinalDate" ]];
   then
           committedToday=true;
