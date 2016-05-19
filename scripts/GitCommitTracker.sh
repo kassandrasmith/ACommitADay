97366f3796b42af0ff2d9fa76a58e2b0c6a26cb4
kassandrasmith <kassandrasmith@utexas.edu>
2016-05-18 23:56:30 -0500

    Reword explanation

--- scripts/GitCommitTracker.sh
+++ scripts/GitCommitTracker.sh
@@ -2,6 +2,8 @@
 committedToday=false;
 #find all git repositories and put them into this text file
 
+  USER=$(sed -n '3p' < ~/.Message.txt)
+  # echo $USER
 # find ~ -name .git -type d -prune 2>/dev/null  > RepositoryDirectory.txt
 #
 # #add globbers for files with a newline
@@ -13,57 +15,61 @@ cat RepositoryDirectory.txt |
 (while read data
 do
 
-cd #not necessary, but a safeguard to get to root
+cd #not necessary, but a safeguard to start at HOME
   cd $data;
   cd .. #compensate for .git at end of line
 
-  LOG_DATE=$(git log -1 HEAD --pretty=format:"%cd" --date=short)
+  LOG_DATE=$(git log --author=kassandrasmith@utexas.edu -1 HEAD --pretty=format:"%cd" --date=short)
 
-  if [[ "$OSTYPE" == "linux-gnu" ]]; then
+  if [ -z "$LOG_DATE" ]
+  then
 
+    if [[ "$OSTYPE" == "linux-gnu" ]]; then
+
+              DATE=$(date +%Y-%m-%d) &> /dev/null #Silence date output
+              StartDate=$(date -d "$DATE" +"%s")
+              FinalDate=$(date -d "$LOG_DATE" +"%s")
+
+    elif [[ "$OSTYPE" == "darwin"* ]]; then
+            # Mac OSX
+              DATE=$(date +%Y-%m-%d) &> /dev/null #Silence date output
+              StartDate=$(date -jf "%Y-%m-%d" "$DATE" +"%s")
+              FinalDate=$(date -jf "%Y-%m-%d" "$LOG_D ATE" +"%s")
+
+
+    elif [[ "$OSTYPE" == "cygwin" ]]; then
+            # POSIX compatibility layer and Linux environment emulation for Windows
+            echo cygwin not yet tested, results may be incorrect;
             DATE=$(date +%Y-%m-%d) &> /dev/null #Silence date output
             StartDate=$(date -d "$DATE" +"%s")
             FinalDate=$(date -d "$LOG_DATE" +"%s")
 
-  elif [[ "$OSTYPE" == "darwin"* ]]; then
-          # Mac OSX
+    elif [[ "$OSTYPE" == "msys" ]]; then
+            # Lightweight shell and GNU utilities compiled for Windows (part of MinGW)
+            echo mysys not yet tested, results may be incorrect;
+            DATE=$(date +%Y-%m-%d) &> /dev/null #Silence date output
+            StartDate=$(date -d "$DATE" +"%s")
+            FinalDate=$(date -d "$LOG_DATE" +"%s")
+
+    elif [[ "$OSTYPE" == "win32" ]]; then
+            # I'm not sure this can happen.
+            echo Win32 not supported.
+    elif [[ "$OSTYPE" == "freebsd"* ]]; then
+            # ...
             DATE=$(date +%Y-%m-%d) &> /dev/null #Silence date output
             StartDate=$(date -jf "%Y-%m-%d" "$DATE" +"%s")
             FinalDate=$(date -jf "%Y-%m-%d" "$LOG_DATE" +"%s")
+    else
+            # Unknown.
+            echo
+    fi
 
-
-  elif [[ "$OSTYPE" == "cygwin" ]]; then
-          # POSIX compatibility layer and Linux environment emulation for Windows
-          echo cygwin not yet tested, results may be incorrect;
-          DATE=$(date +%Y-%m-%d) &> /dev/null #Silence date output
-          StartDate=$(date -d "$DATE" +"%s")
-          FinalDate=$(date -d "$LOG_DATE" +"%s")
-
-  elif [[ "$OSTYPE" == "msys" ]]; then
-          # Lightweight shell and GNU utilities compiled for Windows (part of MinGW)
-          echo mysys not yet tested, results may be incorrect;
-          DATE=$(date +%Y-%m-%d) &> /dev/null #Silence date output
-          StartDate=$(date -d "$DATE" +"%s")
-          FinalDate=$(date -d "$LOG_DATE" +"%s")
-
-  elif [[ "$OSTYPE" == "win32" ]]; then
-          # I'm not sure this can happen.
-          echo Win32 not supported.
-  elif [[ "$OSTYPE" == "freebsd"* ]]; then
-          # ...
-          DATE=$(date +%Y-%m-%d) &> /dev/null #Silence date output
-          StartDate=$(date -jf "%Y-%m-%d" "$DATE" +"%s")
-          FinalDate=$(date -jf "%Y-%m-%d" "$LOG_DATE" +"%s")
-  else
-          # Unknown.
-          echo
-  fi
-
-  if [[ "$StartDate" -eq "$FinalDate" ]];
-  then
-          committedToday=true;
-          whereCommit=$data
-   fi;
+    if [[ "$StartDate" -eq "$FinalDate" ]];
+    then
+            committedToday=true;
+            whereCommit=$data
+     fi
+   fi
 done
 
 
@@ -74,7 +80,7 @@ if [ "$committedToday" = false ] ; then
   else
     #!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!#
     echo $(sed -n '1  p' < ~/.Message.txt)
-    # echo "You've committed today" # "to" $whereCommit #feel free to change me!
+     echo "You've committed today"  "to" $whereCommit #feel free to change me!
 
 fi
 )
